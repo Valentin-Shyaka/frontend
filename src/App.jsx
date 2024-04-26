9f71ebdc3e884729b188b6d76ba9841f119b04d7
Valentin-Shyaka <vava1lantern7@gmail.com>
2024-04-26 10:22:06 +0200

    dashboard route implemented...

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route to={'/login'} element={Login}/>
-    //   </Routes>
-    // </BrowserRouter>
-    <><Login/></>
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/adminLogin'} element={<Login/>}></Route>
+        <Route path={'/dashboard'} element={<Dashboard/>}/>
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
